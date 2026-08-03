7e05310037f09cc85148e0e7b994ac82a403ad5e
Dovie <dovieshapiro@gmail.com>
2026-08-03 07:07:51 -0400

    Repair music toggle icon

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // @ts-expect-error Vite supports query-string imports used to invalidate tunnel caches.
-import { App } from "./App.tsx?v=flat-audio-icons-45";
-import "./styles.css?v=flat-audio-icons-45";
+import { App } from "./App.tsx?v=music-icon-fix-46";
+import "./styles.css?v=music-icon-fix-46";
 
 const root = document.querySelector<HTMLDivElement>("#root");
 
